16b1062e57161eaaf6cfa586aec196b29201ba3d
bendera <szoftverhiba@gmail.com>
2024-10-13 00:48:43 +0200

    Add event type

--- packages/lib/src/components/VscodeTabs.ts
+++ packages/lib/src/components/VscodeTabs.ts
@@ -1,13 +1,14 @@
 import React from "react";
-import { createComponent } from "@lit/react";
+import { createComponent, type EventName } from "@lit/react";
 import { VscodeTabs as VscodeTabsWebComponent } from "@vscode-elements/elements";
+import { VscTabsSelectEvent } from "@vscode-elements/elements/dist/vscode-tabs/vscode-tabs";
 
 const VscodeTabs = createComponent({
   tagName: "vscode-tabs",
   elementClass: VscodeTabsWebComponent,
   react: React,
   events: {
-    onVscTabsSelect: "vsc-tabs-select",
+    onVscTabsSelect: "vsc-tabs-select" as EventName<VscTabsSelectEvent>,
   },
   displayName: "VscodeTabs",
 });
