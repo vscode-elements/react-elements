98ec2f17a75f8dd24e2165bed82266727459f641
bendera <szoftverhiba@gmail.com>
2024-10-22 22:42:00 +0200

    Add events to Textfield and Textarea

--- packages/lib/src/components/VscodeTextfield.ts
+++ packages/lib/src/components/VscodeTextfield.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { createComponent } from "@lit/react";
+import { createComponent, EventName } from "@lit/react";
 import { VscodeTextfield as WC } from "@vscode-elements/elements";
 
 const VscodeTextfield = createComponent({
@@ -7,6 +7,11 @@ const VscodeTextfield = createComponent({
   elementClass: WC,
   react: React,
   displayName: "VscodeTextfield",
+  events: {
+    onChange: "change" as EventName<Event>,
+    onInput: "input" as EventName<InputEvent>,
+    onInvalid: "invalid" as EventName<Event>,
+  },
 });
 
 export default VscodeTextfield;
