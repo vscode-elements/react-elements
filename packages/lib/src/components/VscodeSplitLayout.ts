767588beb2abb4bef73eaa3f48d56a3540347fcc
bendera <szoftverhiba@gmail.com>
2024-11-09 14:34:00 +0100

    Add vsc change event

--- packages/lib/src/components/VscodeSplitLayout.ts
+++ packages/lib/src/components/VscodeSplitLayout.ts
@@ -1,12 +1,17 @@
 import React from "react";
-import { createComponent } from "@lit/react";
+import { createComponent, EventName } from "@lit/react";
 import { VscodeSplitLayout as WC } from "@vscode-elements/elements";
+import { VscSplitLayoutChangeEvent } from "@vscode-elements/elements/dist/vscode-split-layout/vscode-split-layout";
 
 const VscodeSplitLayout = createComponent({
   tagName: "vscode-split-layout",
   elementClass: WC,
   react: React,
   displayName: "VscodeSplitLayout",
+  events: {
+    onVscSplitLayoutChange:
+      "vsc-split-layout-change" as EventName<VscSplitLayoutChangeEvent>,
+  },
 });
 
 export default VscodeSplitLayout;
