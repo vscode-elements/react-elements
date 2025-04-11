ee66317f924f478dd08b2582349c743f65ba16a7
bendera <szoftverhiba@gmail.com>
2025-04-11 23:47:49 +0200

    Show React version

--- packages/demo/src/routes/Root.tsx
+++ packages/demo/src/routes/Root.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import "./Root.css";
 
@@ -6,18 +7,37 @@ export default function Root() {
     <div className="Root">
       <aside>
         <ul>
-          <li><a href="/badge">Badge</a></li>
-          <li><a href="/checkbox">Checkbox</a></li>
-          <li><a href="/multi-select">Multi Select</a></li>
-          <li><a href="/radio">Radio</a></li>
-          <li><a href="/single-select">Single Select</a></li>
-          <li><a href="/split-layout">Split Layout</a></li>
-          <li><a href="/table">Table</a></li>
-          <li><a href="/textarea">Textarea</a></li>
-          <li><a href="/textfield">Textfield</a></li>
+          <li>
+            <a href="/badge">Badge</a>
+          </li>
+          <li>
+            <a href="/checkbox">Checkbox</a>
+          </li>
+          <li>
+            <a href="/multi-select">Multi Select</a>
+          </li>
+          <li>
+            <a href="/radio">Radio</a>
+          </li>
+          <li>
+            <a href="/single-select">Single Select</a>
+          </li>
+          <li>
+            <a href="/split-layout">Split Layout</a>
+          </li>
+          <li>
+            <a href="/table">Table</a>
+          </li>
+          <li>
+            <a href="/textarea">Textarea</a>
+          </li>
+          <li>
+            <a href="/textfield">Textfield</a>
+          </li>
         </ul>
       </aside>
       <main>
+        <p>React version: {React.version}</p>
         <Outlet />
       </main>
     </div>
